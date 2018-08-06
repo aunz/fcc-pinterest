57b9df7fe517a079b13a970359eef245cf845aa7
aunz <aunz10@yahoo.com>
2018-08-06 18:38:02 +1200

    user table

--- src/server/db/createTable/createTable.sql
+++ src/server/db/createTable/createTable.sql
@@ -1,11 +1,12 @@
 create table "user" (
-  id int not null primary key, --convert to base36 when sent to client
+  id int not null primary key,
   ts int not null default (strftime('%s','now')), -- created date, timestamp in epoch
-  "name" text not null check(trim(name) <> ''),
-  email text not null unique check(trim(email) <> ''), -- all lovercase
-  loc text,
-  pw text not null, -- password
-  token blob unique, -- hashed token for user authentication, very likely unique
+  "name" text,
+  email text unique check(trim(email) <> ''), -- all lowercase
+  gh int unique, -- github id, not login
+  gh_name text unique, -- github name
+  pw text, -- password
+  token blob, -- hashed token 256 bits for user authentication, astronomically rare to collide, no need unique constraint
   token_ts int, -- ts when token is created
   token_ts_exp int -- ts when token SHOULD be expired
 );
