c4fb5cc1dce56d7487308aed12bb5666c2780b31
aunz <aunz10@yahoo.com>
2018-08-09 21:18:51 +1200

    check contrains

--- src/server/db/createTable/createTable.sql
+++ src/server/db/createTable/createTable.sql
@@ -2,20 +2,21 @@ create table "user" (
   id int not null primary key,
   ts int not null default (strftime('%s','now')), -- created date, timestamp in epoch
   "name" text,
-  email text unique check(trim(email) <> ''), -- all lowercase
+  email text check(email REGEXP '^[^@\s]+@[^@\s]+\.[^@\s]+$'), 
   gh int unique, -- github id, not login
-  gh_name text unique, -- github name
+  gh_name text check(length(trim(email)) > 2), -- github name
   pw text, -- password
   token blob, -- hashed token 256 bits for user authentication, astronomically rare to collide, no need unique constraint
   token_ts int, -- ts when token is created
   token_ts_exp int -- ts when token SHOULD be expired
 );
-
+create unique index user_email on "user"(lower(email));
+create unique index user_gh_name on "user"(lower(trim(gh_name)));
 
 create table pin (
   id int not null primary key,
   ts int not null default (strftime('%s','now')),
   uid int not null references "user" (id),
-  "name" tex, -- the title of the pin
-  url text not null
+  title text, -- the title of the pin
+  url text not null check(trim(url) <> '')
 ;
--- src/server/db/createTable/createTable.test.js
+++ src/server/db/createTable/createTable.test.js
@@ -6,20 +6,57 @@ const s = { skip: true } // eslint-disable-line
 
 test('init', t => {
   // create user
-  db.prepare('insert into "user" (id, email, pw) values (?, ?, ?)').run(1, 'e1', 'p')
-  db.prepare('insert into "user" (id, email, pw) values (?, ?, ?)').run(2, 'e2', 'p')
-  db.prepare('insert into "user" (id, email, pw) values (?, ?, ?)').run(3, 'e3', 'p')
-  db.prepare('insert into "user" (id, email, pw) values (?, ?, ?)').run(4, 'e4', 'p')
-  db.prepare('insert into "user" (id, email, pw) values (?, ?, ?)').run(5, 'e5', 'p')
-  db.prepare('insert into "user" (id, email, pw) values (?, ?, ?)').run(6, 'e6', 'p')
+  db.prepare('insert into "user" (id) values (?)').run(1)
+  db.prepare('insert into "user" (id, email) values (?, ?)').run(2, 't@test.com')
+  db.prepare('insert into "user" (id, gh) values (?, ?)').run(3, 123)
+  db.prepare('insert into "user" (id, gh_name) values (?, ?)').run(4, 'gh1')
 
   // create pin
-  db.prepare('insert into pin (id, uid, name, url) values (?, ?, ?, ?)').run(1, 1, '', 'url1')
-  db.prepare('insert into pin (id, uid, name, url) values (?, ?, ?, ?)').run(2, 1, null, 'url2')
-  db.prepare('insert into pin (id, uid, name, url) values (?, ?, ?, ?)').run(3, 2, '', 'url3')
-  db.prepare('insert into pin (id, uid, name, url) values (?, ?, ?, ?)').run(4, 2, null, 'url4')
-  db.prepare('insert into pin (id, uid, name, url) values (?, ?, ?, ?)').run(5, 3, '', 'url5')
-  db.prepare('insert into pin (id, uid, name, url) values (?, ?, ?, ?)').run(6, 3, null, 'url6')
+  db.prepare('insert into pin (id, uid, title, url) values (?, ?, ?, ?)').run(1, 1, '', 'url1')
+  db.prepare('insert into pin (id, uid, title, url) values (?, ?, ?, ?)').run(2, 1, null, 'url2')
+  db.prepare('insert into pin (id, uid, title, url) values (?, ?, ?, ?)').run(3, 2, '', 'url3')
+  db.prepare('insert into pin (id, uid, title, url) values (?, ?, ?, ?)').run(4, 2, null, 'url4')
+  db.prepare('insert into pin (id, uid, title, url) values (?, ?, ?, ?)').run(5, 3, '', 'url5')
+  db.prepare('insert into pin (id, uid, title, url) values (?, ?, ?, ?)').run(6, 3, null, 'url6')
 
+  t.end()
+})
+
+test('constrain', t => {
+  t.throw(() => {
+    db.prepare('insert into "user" (id, email) values (?, ?)').run(100, 'T@Test.COM')
+  }, /unique/i, 'email has to be unique')
+
+  t.throw(() => {
+    db.prepare('insert into "user" (id, email) values (?, ?)').run(100, '   t@t  ')
+  }, /unique/i, 'email has to match email regexp')
+
+  t.throw(() => {
+    db.prepare('insert into "user" (id, email) values (?, ?)').run(100, '   t.t.com  ')
+  }, /unique/i, 'email has to match email regexp')
+
+  t.throw(() => {
+    db.prepare('insert into "user" (id, gh) values (?, ?, ?)').run(100, 123)
+  }, /unique/i, 'gh id has to be unique')
+
+  t.throw(() => {
+    db.prepare('insert into "user" (id, gh_name) values (?, ?, ?)').run(100, '  GH1  ')
+  }, /unique/i, 'gh_name has to be unique')
+
+  t.throw(() => {
+    db.prepare('insert into "user" (id, gh_name) values (?, ?)').run(100, '  gh ')
+  }, /unique/i, 'gh_name is not long enough')
+
+  t.throw(() => {
+    db.prepare('insert into pin (id, url) values (?, ?)').run(100, 'u1')
+  }, /unique/i, 'uid is required for pin')
+
+  t.throw(() => {
+    db.prepare('insert into pin (id, uid) values (?, ?)').run(100, 100)
+  }, /unique/i, 'uid needs to references "user"')
+
+  t.throw(() => {
+    db.prepare('insert into pin (id, uid) values (?, ?)').run(100, 1)
+  }, /unique/i, 'url cannot be null')
   t.end()
 })
