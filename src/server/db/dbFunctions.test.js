4b929367ce7579f788164e186b57b30a23c969bb
aunz <aunz10@yahoo.com>
2018-08-11 23:55:13 +1200

    pin test

--- src/server/db/dbFunctions.test.js
+++ src/server/db/dbFunctions.test.js
@@ -15,8 +15,8 @@ import {
 
 const s = { skip: true } // eslint-disable-line
 
-test('Create users', async t => {
-  let r = new Array(10).fill(0).map((_, i) => {
+test('create users', async t => {
+  const r = new Array(10).fill(0).map((_, i) => {
     i++
     return createUserWithEmail({
       name: 'U' + i,
@@ -27,7 +27,11 @@ test('Create users', async t => {
 
   await Promise.all(r)
 
-  r = db.prepare('select * from "user" order by rowid').all()
+  t.end()
+})
+
+test('test users', async t => {
+  let r = db.prepare('select * from "user" order by rowid').all()
   t.ok(r[0].id > 10000 && r[1].id >= r[0].id + 10 && r[9].id >= r[8].id + 10 && r[9].id >= r[0].id + 90, `id starts with 10000 and jumps 10 each: ${r[0].id} ~ ${r[9].id}`)
 
   updateUser(r[0].id, { name: null, token: '123', email: 'u0b@test.com' })
@@ -35,12 +39,19 @@ test('Create users', async t => {
 
   t.ok(r.email === 'u0b@test.com' && r.name === null, 'can get user from token')
 
+  r = db.prepare('select * from "user" where id = ?').get(r.id)
+  t.ok(r.token_ts !== null && r.token_ts_exp === r.token_ts + 7776000, 'token_ts and exp are set')
+
+  t.equal(deleteToken('123'), 1, 'can delete token')
+  r = db.prepare('select * from "user" where id = ?').get(r.id)
+  t.ok(r.token === null && r.token_ts === null && r.token_ts_exp === null, 'token and ts can be deleted')
+
   r = await Promise.all([
     getUserWithEmailPW('u2@test.com', '1232'),
     getUserWithEmailPW('u10@test.com', '12310'),
   ])
 
-  t.equal(r[0].token.length, 28, 'has token string of len 28')
+  t.equal(r[0].token.length + ' ' + r[1].token.length, '28 28', 'has token string of len 28')
   t.equal(
     r.map(el => el.id).join(' '),
     [
@@ -65,6 +76,47 @@ test('Create users', async t => {
   r = createUserWithGH({ gh: 123, gh_name: 'gh1' })
   r = getUserWithId(r)
   t.deepEqual(r, { id: r.id, name: null, gh_name: 'gh1', gh: 123, email: null }, 'can create user by github')
+
+  t.end()
+})
+
+test('create pins', async t => {
+  const uids = db.prepare('select id from "user"').pluck().all()
+  new Array(10).fill(0).map((_, i) => {
+    return createPin({
+      uid: uids[i],
+      title: Math.random() > 0.5 ? 'title' + i : null,
+      url: 'url' + i + '.com'
+    })
+  })
+  t.end()
+})
+
+test('test pins', async t => {
+  const uids = db.prepare('select id from "user"').pluck().all()
+
+  let r = db.prepare('select * from pin order by rowid').all()
+  t.ok(r[0].id > 10000 && r[1].id >= r[0].id + 10 && r[9].id >= r[8].id + 10 && r[9].id >= r[0].id + 90, `id starts with 10000 and jumps 10 each: ${r[0].id} ~ ${r[9].id}`)
+
+  t.throws(() => {
+    createPin({ uid: uids[0] })
+  }, /NOT NULL constraint/i, 'url can not be null')
+
+  t.throws(() => {
+    createPin({ uid: uids[0], url: '1111com' })
+  }, /CHECK constraint/i, 'url is invalid')
+
+  t.throws(() => {
+    createPin({ url: 'url1.com' })
+  }, /NOT NULL constraint/i, 'uid is required')
+
+  t.throws(() => {
+    createPin({ uid: 1, url: 'url1.com' })
+  }, /FOREIGN KEY constraint/i, 'uid foreign key')
+
+  t.throws(() => {
+    db.prepare('delete from "user" where id = ?').run(uids[0])
+  }, /FOREIGN KEY constraint/i, 'cannot delete uid due to foreign key constraint')
 
   t.end()
 })
