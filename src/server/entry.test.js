418210dd22d4585e3c7c81dbbbb53ac4de78ec25
aunz <aunz10@yahoo.com>
2018-08-11 21:49:27 +1200

    test dbfunctions

--- src/server/entry.test.js
+++ src/server/entry.test.js
@@ -1,5 +1,5 @@
-import './db/createTable/createTable.test'
-// import './db/dbFunctions.test'
+// import './db/createTable/createTable.test'
+import './db/dbFunctions.test'
 // import './graphql/index.test'
 // import './entry'
 
