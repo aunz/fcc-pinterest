db4c311a2a8139bd49aa584aa3ebc69b84d41a2f
aunz <aunz10@yahoo.com>
2018-08-15 18:32:50 +1200

    start UI

--- src/client/entry.js
+++ src/client/entry.js
@@ -1,18 +1,18 @@
-// import React from 'react'
-// import { render } from 'react-dom'
-// import { ApolloProvider } from 'react-apollo'
-// import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import { render } from 'react-dom'
+import { ApolloProvider } from 'react-apollo'
+import { BrowserRouter } from 'react-router-dom'
 
-// import client from '~/client/apolloClient'
-// import App from './App'
+import client from '~/client/apolloClient'
+import App from './App'
 
-// render(
-//   <ApolloProvider client={client}>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </ApolloProvider>,
-//   document.getElementById('root')
-// )
+render(
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
+  document.getElementById('root')
+)
 
-// if (module.hot) module.hot.accept()
+if (module.hot) module.hot.accept()
