356b6196f92db314150e19b313cb4f9784fa5d21
aunz <aunz10@yahoo.com>
2018-08-16 21:59:33 +1200

    loading spinner

--- src/client/App.js
+++ src/client/App.js
@@ -8,18 +8,20 @@ import { LOCAL_USER } from './apolloClient'
 
 import Header from './components/Header'
 import User from './components/User'
+import { Loading } from './components/common'
 
 class App extends Component {
   render() {
     return (
       <Query query={LOCAL_USER}>
-        {({ data }) => {
+        {({ data, loading }) => {
           const user = data && data.localUser
           return <Fragment>
             <Header loggedIn={!!user} />
             <Switch>
               <Route path='/user' render={() => <User user={user} />} />
             </Switch>
+            {loading && <div className='fixed h1' style={{ top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}><Loading /></div>}
           </Fragment>
         }}
       </Query>
