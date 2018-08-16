a5f42dda6b9fa8f431356db2f1f69c921353b568
aunz <aunz10@yahoo.com>
2018-08-16 20:19:46 +1200

    add Header

--- src/client/App.js
+++ src/client/App.js
@@ -7,14 +7,19 @@ import './styles/index.css'
 import { LOCAL_USER } from './apolloClient'
 
 import Header from './components/Header'
+import User from './components/User'
 
 class App extends Component {
   render() {
     return (
       <Query query={LOCAL_USER}>
-        {() => {
+        {({ data }) => {
+          const user = data && data.localUser
           return <Fragment>
-            <Header />
+            <Header loggedIn={!!user} />
+            <Switch>
+              <Route path='/user' render={() => <User user={user} />} />
+            </Switch>
           </Fragment>
         }}
       </Query>
