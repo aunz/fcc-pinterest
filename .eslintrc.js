64391bd7bc4facebc491c29a44d3394d08404737
aunz <aunz10@yahoo.com>
2018-08-16 22:45:13 +1200

    add rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
 module.exports = {
+  parser: 'babel-eslint',
   extends: ['standard', 'standard-react'],
   rules: {
     'comma-dangle': 0,
     'space-before-function-paren': 0,
     camelcase: 0,
+    curly: 0,
+    'import/no-unresolved': [2, { commonjs: true }],
+    'import/named': 2,
+    'jsx-quotes': ['error', 'prefer-double'],
   }
 }
