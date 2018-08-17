a7ee845b2b337176312565d050f21b1e9f51b1a5
aunz <aunz10@yahoo.com>
2018-08-17 20:07:43 +1200

    add inputClass

--- src/client/components/common.js
+++ src/client/components/common.js
@@ -3,3 +3,5 @@ import React from 'react'
 export function Loading(props) {
   return <span {...props} className={'icon-spin6 animate-spin ' + (props.className || '')} />
 }
+
+export const inputClass = ' p1 outline-none '
