b2f4068a7aa872db2a4ec5b9db5f8e129ef8f5d4
aunz <aunz10@yahoo.com>
2018-08-19 20:54:32 +1200

    Add ErrorMessage component

--- src/client/components/common.js
+++ src/client/components/common.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 
 export function Loading(props) {
   return <span {...props} className={'icon-spin6 animate-spin ' + (props.className || '')} />
+}
+
+export class ErrorMessage extends PureComponent {
+  static propTypes = {
+    error: PropTypes.string
+  }
+  state = {
+    error: this.props.error,
+  }
+  onClick = () => {
+    this.setState({ error: '' })
+  }
+  render() {
+    const { error: _, ...rest } = this.props
+    const { error } = this.state
+    if (!error) return null
+    return (
+      <span
+        className="red"
+        {...rest}
+      >
+        {error}
+        <button
+          className={buttonClass + ' border-none bold red'}
+          onClick={this.onClick}
+        >
+          ✕
+        </button>
+      </span>
+    )
+  }
 }
 
 export const inputClass = ' p1 outline-none '
