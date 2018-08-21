62297d9c8b2e41ba03c35c6f0fd5aee9cb5d236a
aunz <aunz10@yahoo.com>
2018-08-21 18:36:09 +1200

    move loading component to user path

--- src/client/App.js
+++ src/client/App.js
@@ -10,6 +10,8 @@ import Header from './components/Header'
 import User from './components/User'
 import { Loading } from './components/common'
 
+const LoadingDiv = <div className="fixed h1" style={{ top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}><Loading /></div>
+
 class App extends Component {
   render() {
     return (
@@ -19,9 +21,8 @@ class App extends Component {
           return <Fragment>
             <Header loggedIn={!!user} />
             <Switch>
-              <Route path="/user" render={() => <User user={user} />} />
+              <Route path="/user" render={() => loading ? LoadingDiv : <User user={user} />} />
             </Switch>
-            {loading && <div className="fixed h1" style={{ top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}><Loading /></div>}
           </Fragment>
         }}
       </Query>
